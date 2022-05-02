8d5e6d775200d89904b540d7f7b78ec8a6dad8bd
Dmitriy Sevostyanov <dim.sewostyanow2011@gmail.com>
2022-05-02 19:53:46 +0300

    feat: add routes to ml model

--- server/apps/endpoints/urls.py
+++ server/apps/endpoints/urls.py
@@ -1,5 +1,5 @@
 # backend/server/apps/endpoints/urls.py file
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 
 from . import views
@@ -7,9 +7,9 @@ from . import views
 router = DefaultRouter(trailing_slash=False)
 router.register(r"endpoints", views.EndpointViewSet, basename="endpoints")
 router.register(r"mlalgorithms", views.MLAlgorithmViewSet, basename="mlalgorithms")
-router.register(r"mlalgorithmstatuses", views.MLAlgorithmStatusViewSet, basename="mlalgorithmstatuses")
 router.register(r"mlrequests", views.MLRequestViewSet, basename="mlrequests")
 
 urlpatterns = [
     path(r"api/v1/", include(router.urls)),
+    path(r"api/v1/<endpoint_name>/predict", views.PredictView.as_view(), name="predict")
 ]
